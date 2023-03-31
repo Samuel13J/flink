cc93f111e19fae52b619e5cebe06e1e392ef9d7c
Samuel13J <fjdswj@163.com>
2023-03-31 17:03:45 +0800

    flink消费kafka数据写入mysql中

---
+++ src/main/java/com/jaywong/flink/MysqlImpl.java
@@ -0,0 +1,6 @@
+package com.jaywong.flink;/**
+@author wangjie
+@create 2023-03-30 18:10
+*/
+    public class MysqlImpl {
+}
---
+++ src/main/java/com/jaywong/flink/PropertiesUtils.java
@@ -0,0 +1,6 @@
+package com.jaywong.flink;/**
+@author wangjie
+@create 2023-03-30 18:08
+*/
+    public class PropertiesUtils {
+}
